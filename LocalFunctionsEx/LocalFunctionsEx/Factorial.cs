e91fbb19bab64ead0c2a7e8054e19a408bceac9c
saikiran-5260 <119442185+saikiran-5260@users.noreply.github.com>
2022-11-29 23:26:40 +0530

    Add files via upload

---
+++ LocalFunctionsEx/LocalFunctionsEx/Factorial.cs
@@ -0,0 +1,27 @@
+﻿class Sample
+{
+    public int Factorial(int num )
+    {
+        if(num == 0)
+        {
+            return 1;
+        }
+        else
+        {
+            return num * Factorial(num-1);
+        }
+    }
+}
+class Factorial
+{
+    public static void Main()
+    {
+        Sample s = new Sample();
+
+        Console.Write("Enter Number: ");
+        int number = int.Parse(Console.ReadLine());
+        double fact = s.Factorial(number);
+        Console.WriteLine("Factorial is: " + fact);
+        Console.ReadKey();
+    }
+}
---
+++ LocalFunctionsEx/LocalFunctionsEx/Program.cs
@@ -0,0 +1,31 @@
+﻿class StudentMarks
+{
+    public void getMarks(params int[] marks )
+    {
+        double avgMarks = calculateAvg( marks );
+        Console.WriteLine("Avg marks: " + avgMarks);
+
+
+        static double calculateAvg(int[] marks)
+        {
+            int totalMarks = 0;
+            for (int i = 0; i < marks.Length; i++)
+            {
+                totalMarks += marks[i];
+                Console.WriteLine($"marks{i + 1}: " + marks[i]);
+            }
+            double avg;
+            avg = (double) (totalMarks)/marks.Length;
+            return avg;
+        }
+    }
+}
+class Program
+{
+    static void Main()
+    {
+        StudentMarks s = new StudentMarks();
+        s.getMarks(80,45,71);
+        Console.ReadKey();
+    } 
+}
