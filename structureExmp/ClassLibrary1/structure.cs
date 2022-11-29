0d062ab95c1f601f423cc27024122c1c69c0b507
saikiran-5260 <119442185+saikiran-5260@users.noreply.github.com>
2022-11-29 23:37:23 +0530

    Add files via upload

---
+++ structureExmp/ClassLibrary1/structure.cs
@@ -0,0 +1,44 @@
+﻿namespace ClassLibrary1
+{
+    public struct structure
+    {
+        private int _pin;
+        private string _accountHolderName;
+
+        public int GetPin
+        {
+
+            set
+                {
+                    _pin = value;
+                }
+          
+              
+            get
+            {
+                return _pin;
+            }
+        }
+
+        public string AccountHolderName
+        {
+            
+        set
+        {
+            _accountHolderName = value;
+        }
+          
+           
+        get
+        {
+            return _accountHolderName;
+        }
+        }
+
+    }
+}
+
+public class User<T>
+{
+    public T RegistrationStatus;
+}
---
+++ structureExmp/structureExmp/Program.cs
@@ -0,0 +1,20 @@
+﻿using ClassLibrary1;
+
+structure s = new structure();
+s.GetPin = 230;
+s.AccountHolderName = "shanmukha sai kiran Pithani";
+
+Console.WriteLine("account holder name is: "+ s.AccountHolderName);
+Console.WriteLine("Pin updated is: " + s.GetPin);
+
+
+User<string> user = new User<string>();
+user.RegistrationStatus = "1234";
+Console.WriteLine(user.RegistrationStatus.Length);
+Console.WriteLine(user.RegistrationStatus.GetType());
+
+
+User<int> user1 = new User<int>();
+user1.RegistrationStatus = 1234;
+Console.WriteLine(user1.RegistrationStatus);
+Console.WriteLine(user1.RegistrationStatus.GetType());
