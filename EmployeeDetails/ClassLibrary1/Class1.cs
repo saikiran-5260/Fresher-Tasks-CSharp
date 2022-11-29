8e030052d5538a309e295d6d84e7d83a46b586c9
saikiran-5260 <119442185+saikiran-5260@users.noreply.github.com>
2022-11-29 23:24:43 +0530

    Add files via upload

---
+++ EmployeeDetails/ClassLibrary1/Class1.cs
@@ -0,0 +1,16 @@
+﻿public class Employee
+{
+    public int EmpId;
+    public string EmpName;
+    public int SalaryPerHour;
+    public int NoOfHoursWorking;
+    public int NetSalary;
+    public static string OrganizationName = "Shanmukha Sai Kiran Inc";
+    public const string TypeOfEmployee = "Contract Based";
+    public readonly string DepartmentName = "Finance Department";
+
+    //public Employee()
+    //{
+    //    public readonly string DepartmentName = "Finance Department";
+    //}
+}
---
+++ EmployeeDetails/EmployeeDetails/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+class EmployeeProject
+{
+    static void Main()
+    {
+        Console.WriteLine($"****************{ Employee.OrganizationName }****************");
+        Employee.OrganizationName = "Harsha INC";
+        //Employee.DepartmentName = "ROD";
+        Console.WriteLine($"{Employee.TypeOfEmployee}");
+        for (int i = 0; i < 10; i++)
+        {
+            string EmployeeNumber;
+            switch (i)
+            {
+                case 0: EmployeeNumber = "First Emp"; break;
+                case 1: EmployeeNumber = "Second Emp"; break;
+                case 2: EmployeeNumber = "Third Emp"; break;
+                case 3: EmployeeNumber = "Fourth Emp"; break;
+                case 4: EmployeeNumber = "Fifth Emp"; break;
+                case 5: EmployeeNumber = "Sixth Emp"; break;
+                case 6: EmployeeNumber = "Seventh Emp"; break;
+                case 7: EmployeeNumber = "Eighth Emp"; break;
+                case 8: EmployeeNumber = "Ninth Emp"; break;
+                case 9: EmployeeNumber = "Tenth Emp"; break;
+                default : EmployeeNumber = "Other Emp"; break;
+            }
+            Console.WriteLine(EmployeeNumber);
+            Employee emp = new Employee();
+            Console.Write("Enter EmpId:");
+            emp.EmpId = Convert.ToInt16(Console.ReadLine());
+            Console.Write("Enter EmpName:");
+            emp.EmpName = Console.ReadLine();
+            Console.Write("Enter Employee salaryPerHour:");
+            emp.SalaryPerHour = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter NoOfWorking hours of employee:");
+            emp.NoOfHoursWorking = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Employee department:" + emp.DepartmentName);
+            emp.NetSalary = emp.SalaryPerHour * emp.NoOfHoursWorking;
+            Console.WriteLine($"Employee NetSalary is: {emp.NetSalary}");
+            Console.WriteLine("Do you want to proceed with the next employee? choose your choice: y/n");
+            string choice = Console.ReadLine(); ;
+            if (choice == "no" || choice == "n" || choice == "N")
+            {
+                break;
+            }   
+            else if (choice == "Yes" || choice == "Y" || choice == "YES" || choice == "y")
+            {
+                continue;
+            }
+            else
+            {
+                break;
+            }
+        }
+        Console.ReadKey();
+    }
+   
+}
