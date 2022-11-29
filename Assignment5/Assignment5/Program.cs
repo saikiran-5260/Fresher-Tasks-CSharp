025b606f91264c61e04c3c439f3561ede12e006b
saikiran-5260 <119442185+saikiran-5260@users.noreply.github.com>
2022-11-29 23:20:51 +0530

    Add files via upload

---
+++ Assignment5/Assignment5/Program.cs
@@ -0,0 +1,23 @@
+﻿
+class Assignment5
+{
+    static void Main()
+    {
+        Number a = new Number();
+        a.SetValue(2);
+        System.Console.WriteLine("Number: " + a.GetValue());
+        System.Console.WriteLine("Is Number is Zero: " + a.IsZero());
+        System.Console.WriteLine("Is Number is Positive: " + a.IsPositive());
+        System.Console.WriteLine("Is Number is Positive: " + a.IsNegative());
+        System.Console.WriteLine("Is Number is Prime: " + a.IsOdd());
+        System.Console.WriteLine("Is Number is Prime: " + a.IsEven());
+        System.Console.WriteLine("Is Number is Prime: " + a.IsPrime());
+        System.Console.WriteLine("Is Number is Prime: " + a.IsPrime());
+        System.Console.WriteLine("Count of Value: " + a.GetCountOfDigits());
+        Number b = new Number();
+        System.Console.WriteLine(type(b.Value));
+
+        System.Console.ReadKey();
+    }
+    
+}
---
+++ Assignment5/ClassLibrary1/Class1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+
+public class Number
+{
+    private int Value;
+
+    public void SetValue(int value)
+    {
+        Value = value;
+    }
+    public double GetValue()
+    {
+        return Value;
+    }
+    public bool IsZero()
+    {
+        if (Value == 0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    public bool IsPositive()
+    {
+        if (Value < 0)
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+
+    public bool IsNegative()
+    {
+        if (Value < 0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public bool IsOdd()
+    {
+        if (Value % 3 == 0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    public bool IsEven()
+    {
+        if (Value % 2 == 0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    public bool IsPrime()
+    {
+        int count = 0;
+        for (int i = 1; i <= Value; i++)
+        {
+            if (Value % i == 0)
+            {
+                count++;
+            }
+        }
+        if (count == 2)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    public static int Power(int baseNumber, int exponent, bool recursive = false)
+    {
+        if (!recursive) //if not recursive, perform a normal 'for' loop
+        {
+            int power = baseNumber;
+            for (int i = 1; i < exponent; i++) //loop for 'exponent - 1' times
+            {
+                power *= baseNumber; //multiply the baseNumber itself
+            }
+            return power;
+        }
+        else //if recursive
+        {
+            if (exponent == 0) //if the exponent is reached down to zero (0), then the result is 1.
+                return 1;
+            else //if the exponent is greater than zero (0), then the result is 'baseNumber * power(basenumber, exponent - 1)'.
+                return baseNumber * (Number.Power(baseNumber: baseNumber, exponent: exponent - 1, recursive: true));
+        }
+    }
+    public int GetCountOfDigits()
+    {
+        
+    }
+}
