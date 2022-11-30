b9605d39583ba6f7a023019ba11fff2bf21390f3
saikiran-5260 <119442185+saikiran-5260@users.noreply.github.com>
2022-11-30 18:28:27 +0530

    Add files via upload

---
+++ ExtententionMethod/ExtentionMethodClass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using ClassLibrary1;
+
+namespace ExtentionMethodExmp
+{
+    public static class ProductExtention
+    {
+        public static double GetDiscount(this Product product)
+        {
+            return product.productCost * product.productDiscount / 100;
+        }
+    }
+}
