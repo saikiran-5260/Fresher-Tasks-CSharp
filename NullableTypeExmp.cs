8f2e235c3c0bc9586e6ba2669b62751121854699
saikiran-5260 <119442185+saikiran-5260@users.noreply.github.com>
2022-11-30 12:41:27 +0530

    Add files via upload

---
+++ NullableTypeExmp.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace namespace1
+{
+    class Person
+    {
+        public int? NoOfChildren;
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            Person p1 = new Person() { NoOfChildren = 2 };
+            Person p2 = new Person() { NoOfChildren = null };
+            Person p3 = new Person() { NoOfChildren = null };
+            Person p4 = new Person() { NoOfChildren = 5 };
+
+            Console.WriteLine(p1.NoOfChildren); 
+
+            if (p2.NoOfChildren.HasValue)  // HasValue is false if field value is null
+            {
+                Console.WriteLine(p2.NoOfChildren);
+            }
+            else
+            {
+                Console.WriteLine("null");
+            }
+
+            Console.WriteLine(p3.NoOfChildren ?? 0); //null coalescing
+
+            Console.WriteLine(p4?.NoOfChildren); // null propagation
+        }
+    }
+}
