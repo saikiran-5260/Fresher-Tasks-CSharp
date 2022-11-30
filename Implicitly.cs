a216e2a4ae3cb49e72e64b665c19c2bc87c3cf2c
saikiran-5260 <119442185+saikiran-5260@users.noreply.github.com>
2022-11-30 16:14:46 +0530

    Add files via upload

---
+++ Implicitly.cs
@@ -0,0 +1,32 @@
+﻿
+// Implicitly type variable with keyword "var"
+
+using namespace1.namespace2.namespace3;
+
+namespace namespace1
+{
+    namespace namespace2
+    {
+        namespace namespace3
+        {
+            public class ImplicitlyTypeConvertion
+            {
+                public int userName { set; get; }
+            }
+        }
+    }
+}
+
+
+class User
+{
+    static void Main()
+    {
+        namespace1.namespace2.namespace3.ImplicitlyTypeConvertion p = new namespace1.namespace2.namespace3.ImplicitlyTypeConvertion();
+        Console.WriteLine(p.GetType());
+
+        // Instead of above statement this helps in deciding the type of reference variable without declaring nested namespaces
+        var p1 = new namespace1.namespace2.namespace3.ImplicitlyTypeConvertion();
+        Console.WriteLine(p1.GetType());
+    }
+}
