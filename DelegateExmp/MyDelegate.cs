e4b3af614b573a649c67f9010a34b520ebe12faf
saikiran-5260 <119442185+saikiran-5260@users.noreply.github.com>
2022-12-01 11:15:19 +0530

    Add files via upload

---
+++ DelegateExmp/MyDelegate.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public delegate double MyDelegateType(double a, double b);
+}
