9aff7b0833e560211f82a8dd6bb725ca01df3197
saikiran-5260 <119442185+saikiran-5260@users.noreply.github.com>
2022-12-01 11:16:22 +0530

    Add files via upload

---
+++ DelegateExmpSingleCast/Sample.cs
@@ -0,0 +1,11 @@
+﻿namespace ClassLibrary1
+{
+    public class Sample
+    {
+        public double DiscountCal(double mrp, double discountPrice)
+        {
+            double discountPercentage = (mrp - discountPrice)/mrp * 100;
+            return discountPercentage;
+        }
+    }
+}
