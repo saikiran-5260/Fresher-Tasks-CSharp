33e1252c3fda7fcd31fef87deded1ffdff71dd6f
saikiran-5260 <119442185+saikiran-5260@users.noreply.github.com>
2022-12-01 11:16:00 +0530

    Delete DelegateExmp directory

--- DelegateExmp/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using ClassLibrary1;
-
-Sample s = new Sample();
-
-
-MyDelegateType myDelegate =new MyDelegateType(s.DiscountCal);
-
-Console.WriteLine(myDelegate.Invoke(2000, 1000));
