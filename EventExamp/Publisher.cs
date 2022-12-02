9bc1900609ef5a25576010c978464b71de5eb7fe
saikiran-5260 <119442185+saikiran-5260@users.noreply.github.com>
2022-12-02 14:46:26 +0530

    Update Publisher.cs

--- EventExamp/Publisher.cs
+++ EventExamp/Publisher.cs
@@ -1,29 +1,65 @@
-﻿namespace ClassLibrary1
+namespace ClassLibrary1
 {
     public class Publisher
     {
-        public delegate void MyDelegateType(int x, int y);
+        public delegate int MyDelegateType(int x, int y);
 
-        private MyDelegateType myDelegate;
+        // Implementing auto implemented event method.
 
-        public event MyDelegateType myEvent
+        //private MyDelegateType myDelegate;  
+        //{
+        //    add 
+        //    { 
+        //        myDelegate += value; 
+        //    }
+        //    remove 
+        //    { 
+        //        myDelegate -= value; 
+        //    }  
+        //}
+
+        //public event MyDelegateType myEvent; 
+
+        /*public event Func<int, int, int> myEvent;*/ //using Func generic type delegate to execute the event.
+
+        //public int RaiseEvent(int a, int b)
+        //{
+        //    if(this.myEvent != null)
+        //    {
+        //        int x = this.myEvent(a, b);
+        //        return x;
+        //    }
+        //    else
+        //    {
+        //        return 0;
+        //    }
+
+        //}
+
+        //public event Action<int, int> myEvent;
+        //public void  RaiseEvent(int a, int b) // action delegate
+        //{
+        //    if (this.myEvent != null)
+        //    {
+        //        this.myEvent(a, b);
+
+        //    }
+
+
+        //}
+   
+    public event Predicate<int> myEvent;
+        public bool RaiseEvent(int a)
         {
-            add 
-            { 
-                myDelegate += value; 
-            }
-            remove 
-            { 
-                myDelegate -= value; 
-            }  
-        }
-        public void RaiseEvent(int a, int b)
-        {
-            if(this.myDelegate != null)
+            if (this.myEvent != null)
             {
-                myDelegate(a, b);
+                bool result = this.myEvent(a);
+                return result;
             }
-            
+            else
+            {
+                return false;
+            }
         }
 
     }
