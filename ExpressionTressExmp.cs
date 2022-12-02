7b87e9fbeffb93fd102db8b6807c6eba609db781
saikiran-5260 <119442185+saikiran-5260@users.noreply.github.com>
2022-12-02 14:49:21 +0530

    Add files via upload

---
+++ ExpressionTressExmp.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq.Expressions;
+
+class Student
+{
+    public int studentId { set; get; }
+    public string studentName { set; get; }
+    public int studentAge { set; get; }
+}
+class Program
+{
+    static void Main()
+    {
+        Student s = new Student() { studentId = 101, studentName = "sai Kiran", studentAge = 21 };
+        Expression<Func<Student, bool>> expression = s => s.studentAge > 12 && s.studentAge < 20;
+
+        Func<Student, bool> myDelegate = expression.Compile();
+        bool resutl = myDelegate.Invoke(s);
+        Console.WriteLine(resutl);
+    }
+}
+
+
