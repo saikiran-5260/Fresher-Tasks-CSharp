236c6675afb824634a1760be3f7d56d03232f649
saikiran-5260 <119442185+saikiran-5260@users.noreply.github.com>
2022-12-02 14:44:52 +0530

    Update Program.cs

--- EventExamp/Program.cs
+++ EventExamp/Program.cs
@@ -1,10 +1,51 @@
-﻿using ClassLibrary1;
+using ClassLibrary1;
 using EventExample;
 
-SubscriberClass subscriber = new SubscriberClass();
+//SubscriberClass subscriber = new SubscriberClass();
 
 Publisher publisher = new Publisher();
 
-publisher.myEvent += subscriber.ADD;
+//publisher.myEvent += subscriber.ADD;
 
-publisher.RaiseEvent(10, 20);
+//publisher.RaiseEvent(10, 20);
+//publisher.RaiseEvent(5, 9);
+//publisher.RaiseEvent(6, 4);
+
+//publisher.myEvent += delegate (int a, int b) // Anonymous method  both methods helps in avoiding subscribe class or creating new class
+//publisher.myEvent += ( a,  b) => //Lambda Expression( => )
+//{
+//    int c = a + b;
+//    Console.WriteLine(c);
+//    return c;
+//};
+
+//publisher.myEvent += (a) => /*a+b;*/ // for action delegate
+//{
+//    int c = a + b;
+//    Console.WriteLine(c);
+
+//};
+
+publisher.myEvent += (a) => // Predicate delegate function
+{
+    return a >= 0;
+};
+
+//publisher.RaiseEvent(10, 20);
+//publisher.RaiseEvent(5, 9);
+//publisher.RaiseEvent(6, 4);
+
+
+Console.WriteLine(publisher.RaiseEvent(10));
+Console.WriteLine(publisher.RaiseEvent(-5));
+Console.WriteLine(publisher.RaiseEvent(-6));
+
+
+
+//Console.WriteLine(publisher.RaiseEvent(10, 20));
+//Console.WriteLine(publisher.RaiseEvent(5, 9));
+//Console.WriteLine(publisher.RaiseEvent(6, 4));
+
+//publisher.RaiseEvent(10,20);
+//publisher.RaiseEvent(5, 9);
+//publisher.RaiseEvent(6, 4);
