8fdd327296c9080fcf23551b0790ca90be066844
saikiran-5260 <119442185+saikiran-5260@users.noreply.github.com>
2022-12-02 14:43:19 +0530

    Add files via upload

---
+++ EventHandlerExmp/Program.cs
@@ -0,0 +1,34 @@
+﻿using ClassLibrary1;
+
+
+class Program
+{
+
+    static void Main()
+    {
+        Program p = new Program();  
+
+        p.doWork();
+    }
+    public void doWork()
+    {
+        Publisher pub = new Publisher();
+
+        pub.myEvent += (sender, e) =>
+        {
+            int c = e.a + e.b;
+            Console.WriteLine(c);
+        };
+
+
+        pub.RaiseEvent(this, 10, 50);
+        pub.RaiseEvent(this, 6, 4);
+        pub.RaiseEvent(this, 10, 20);
+    }
+}
+
+
+
+
+
+
---
+++ EventHandlerExmp/Publisher.cs
@@ -0,0 +1,23 @@
+﻿namespace ClassLibrary1
+{
+    public class CustomEventArgs : EventArgs
+    {
+        public int a { get; set; }
+        public int b { get; set; }  
+    }
+
+    public class Publisher
+    {
+        public EventHandler<CustomEventArgs> myEvent;
+
+        public void RaiseEvent(object sender, int a, int b) 
+        {
+            if(this.myEvent != null)
+            {
+                CustomEventArgs customEventArgs= new CustomEventArgs() { a= a, b= b };
+                this.myEvent(sender, customEventArgs);
+            }
+        }
+    }
+
+}
