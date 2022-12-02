96bead2c9feac821c3487ebb31a5255c2e5b0e3d
saikiran-5260 <119442185+saikiran-5260@users.noreply.github.com>
2022-12-02 15:07:26 +0530

    Add files via upload

---
+++ ExpressionBodiedMemberExamp/Class1.cs
@@ -0,0 +1,20 @@
+﻿namespace ClassLibrary1
+{
+    public class Student
+    {
+        private string _studentName;
+
+        public int GetStudentNameLength() => _studentName.Length;
+
+        public  Student() => _studentName = "sai";
+
+        public string StudentName
+        {
+            set => _studentName = value;
+            get => _studentName;
+        }
+            
+    }
+
+    
+}
---
+++ ExpressionBodiedMemberExamp/Program.cs
@@ -0,0 +1,6 @@
+﻿using ClassLibrary1;
+
+
+Student s = new Student() { StudentName = "kiran"};
+Console.WriteLine(s.StudentName);
+Console.WriteLine(s.GetStudentNameLength());
