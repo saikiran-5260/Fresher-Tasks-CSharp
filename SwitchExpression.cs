ac2b46024f7668b6b5d426e169fe52efe848ea25
saikiran-5260 <119442185+saikiran-5260@users.noreply.github.com>
2022-12-02 15:17:20 +0530

    Add files via upload

---
+++ SwitchExpression.cs
@@ -0,0 +1,14 @@
+﻿
+int operation = Convert.ToInt16(Console.ReadLine()); 
+string result;
+
+result = operation switch
+{
+    1 => "Customer",
+    2 => "Employee",
+    3 => "Supplier",
+    4 => "Distributor",
+    5 => "No case Available",
+
+};
+Console.WriteLine(result);
