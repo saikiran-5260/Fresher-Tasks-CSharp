804e3ab2ef00e2e46e44bac87123c1342505304d
saikiran-5260 <119442185+saikiran-5260@users.noreply.github.com>
2022-12-06 16:45:47 +0530

    Add files via upload

---
+++ IndexFromEndOperator.cs
@@ -0,0 +1,15 @@
+﻿
+
+
+int[] a= new int[6] { 3,5,8,9,10,2};
+
+int result = a[^1];
+Console.WriteLine(result);
+
+//range operator
+int[] result1 = a[2..5];
+
+foreach (int i in result1)
+{
+    Console.WriteLine(i);   
+}
