0a34f4d87c8ee6d6515d3a57d529cee31f460be1
Shanmukha Sai Kiran Pithani <al5260@accionlabs.in>
2022-12-06 22:16:29 +0530

    test adding files

---
+++ ArrayOfObj/ArrayOfObj/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayOfObj
+{
+    public class Employee
+    {
+        public int EmpId;
+        public string EmpName;
+
+    }
+}
---
+++ ArrayOfObj/ArrayOfObjectsExmp/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayOfObj
+{
+    public class Employee
+    {
+        public int EmpId;
+        public string EmpName;
+        public string EmpNamenew;
+
+    }
+}
