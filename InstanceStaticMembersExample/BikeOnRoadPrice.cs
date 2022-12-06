6e5d4fda75207ce62a87b2dd90d6b00e41e498bb
saikiran-5260 <119442185+saikiran-5260@users.noreply.github.com>
2022-12-06 12:54:16 +0530

    Add files via upload

---
+++ InstanceStaticMembersExample/BikeOnRoadPrice.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InstanceStaticMembersExmp
+{
+    public class BikeOnRoadPrice
+    {
+        public int onRoadPrice;
+        public string bikeModelName;
+        public static int bikePrice;
+
+        public BikeOnRoadPrice(int onRoadCost, string bikeModel)
+        {
+            onRoadPrice = onRoadCost;
+            bikeModelName= bikeModel;
+        }
+
+        public void DisplayDetials()
+        {
+            Console.WriteLine("Choose City: ");
+            Console.WriteLine("Hyderabad");
+            Console.WriteLine("Banglore");
+            Console.WriteLine("Chenni");
+            Console.WriteLine("Mumbai");
+            Console.WriteLine("Pune");
+
+            string chooseCity;
+            Console.Write("Please enter the city:");
+            chooseCity = Console.ReadLine();    
+
+            switch(chooseCity)
+            {
+                case "Hyderabad": Console.WriteLine("\nBike price: {0}\nBike Name: {1} \nOn road price: {2}", onRoadPrice, bikeModelName, bikePrice + onRoadPrice);break;
+                case "Banglore": Console.WriteLine("\nBike price: {0}\nBike Name: {1} \nOn road price: {2}", onRoadPrice, bikeModelName, bikePrice + onRoadPrice+2000); break;
+                case "Chenni": Console.WriteLine("\nBike price: {0}\nBike Name: {1} \nOn road price: {2}", onRoadPrice, bikeModelName, bikePrice + onRoadPrice+3000); break;
+                case "Mumbai": Console.WriteLine("\nBike price: {0}\nBike Name: {1} \nOn road price: {2}", onRoadPrice, bikeModelName, bikePrice + onRoadPrice+4000); break;
+                case "Pune": Console.WriteLine("\nBike price: {0}\nBike Name: {1} \nOn road price: {2}", onRoadPrice, bikeModelName, bikePrice + onRoadPrice+5000); break;
+                default: Console.WriteLine("\nCity which is provided does not exist in the list"); break;
+            }
+
+
+            
+        }
+    }
+}
---
+++ InstanceStaticMembersExample/Program.cs
@@ -0,0 +1,7 @@
+﻿using InstanceStaticMembersExmp;
+
+
+
+BikeOnRoadPrice bike = new BikeOnRoadPrice(100000, "RoyalEnfield");
+BikeOnRoadPrice.bikePrice = 16000;
+bike.DisplayDetials();
