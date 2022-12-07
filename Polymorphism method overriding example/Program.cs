dd4980eecec6f00d5a795453d90d55bda9678e90
saikiran-5260 <119442185+saikiran-5260@users.noreply.github.com>
2022-12-07 19:02:18 +0530

    Add files via upload

---
+++ Polymorphism method overriding example/Program.cs
@@ -0,0 +1,9 @@
+﻿
+
+using PolymorphismMethodOverriding;
+
+MethodOverriding c1 = new ChildClass();
+c1.DisplayDetails(100000,"KTM",2000,"Hyderabad");
+
+ChildClass c2 = new ChildClass();
+c2.DisplayDetails(200000, "BENILI", 3000, "Bangalore");
