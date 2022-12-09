92173b87b34eced55f84f58b722c4d2eb9d50c2c
saikiran-5260 <119442185+saikiran-5260@users.noreply.github.com>
2022-12-09 19:07:16 +0530

    Add files via upload

---
+++ CollectionsToArrayForEach.cs
@@ -0,0 +1,14 @@
+﻿
+
+List <int> myList = new List<int> { 12,13,14,15,16,17,18,19,20};
+
+int [] newArr=myList.ToArray();
+
+foreach(int i in newArr)
+{
+    Console.WriteLine(i);
+}
+
+Console.WriteLine("\n");
+List<string> myStringList = new List<string>(4){"sai","Kiran","Shanmukha","Pithani"};
+myStringList.ForEach(el => Console.WriteLine(el));
