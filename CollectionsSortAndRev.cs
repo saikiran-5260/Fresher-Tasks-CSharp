3d90655930906f9f5425bda7012d39055e2a92df
saikiran-5260 <119442185+saikiran-5260@users.noreply.github.com>
2022-12-09 19:11:07 +0530

    Add files via upload

---
+++ CollectionsSortAndRev.cs
@@ -0,0 +1,17 @@
+﻿
+
+
+List<int> myList = new List<int> { 9,6,5,49,21,12,94};
+
+myList.Sort();
+foreach(int i in myList)
+{
+    Console.Write(i+"\t");
+}
+Console.WriteLine("\n");
+
+myList.Reverse();
+foreach(int j in myList)
+{
+    Console.Write(j+"\t");
+}
