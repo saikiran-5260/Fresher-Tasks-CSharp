f35f60660714b0f4a88f95ed07caf845eb0077ea
saikiran-5260 <119442185+saikiran-5260@users.noreply.github.com>
2022-12-09 18:58:53 +0530

    Add files via upload

---
+++ CollectionSplFun.cs
@@ -0,0 +1,17 @@
+﻿
+
+
+List<int> myList = new List<int> { 10, 20,30,40,50,60,70,80,90 };
+
+// IndexOf function
+int Index  = myList.IndexOf(30);
+Console.WriteLine("Index of given element: {0}", Index);
+
+//BinarySearch of myList
+
+int I = myList.BinarySearch(70);
+Console.WriteLine("Binary Search index is: {0} ", I);
+
+//Contains fuction
+bool exits = myList.Contains(40);
+Console.WriteLine("The given exits in the list: {0}",exits);
