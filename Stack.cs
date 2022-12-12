3c9f54d470849e11dd29ee22c93f2673e46dfcc5
saikiran-5260 <119442185+saikiran-5260@users.noreply.github.com>
2022-12-12 21:56:56 +0530

    Add files via upload

---
+++ Stack.cs
@@ -0,0 +1,22 @@
+﻿
+
+
+//Last In First Out
+
+
+Stack<int> s = new Stack<int>();
+s.Push(60); // third 
+s.Push(70); // 70 second
+s.Push(80); // 80 will be printed first              
+
+foreach(int i in s)
+{
+    Console.WriteLine(i);
+}
+
+int p = s.Pop();
+Console.WriteLine("pop ele: "+p); // removes the last element from the stack
+
+int pk =s.Peek();
+Console.WriteLine("peek ele: "+pk); // Gets the last element without deleting.
+
