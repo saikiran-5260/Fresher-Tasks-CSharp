dda045329f8a0d721ab53d020af12059a3865550
saikiran-5260 <119442185+saikiran-5260@users.noreply.github.com>
2022-12-12 18:05:23 +0530

    Add files via upload

---
+++ ParamsParameter modifier/ParamsParameterModifier.cs
@@ -0,0 +1,17 @@
+﻿
+namespace TestProject
+{
+    public class ParamsParameterModifier
+    {
+
+        public void  paramsParameterModifier(params int[] numbers)
+        {
+            int total = 0;
+            foreach(int i in numbers)
+            {
+                 total  =  total+ i;
+            }
+            Console.WriteLine("Sum: {0}",total);
+        }
+    }
+}
---
+++ ParamsParameter modifier/Program.cs
@@ -0,0 +1,8 @@
+﻿
+
+
+using TestProject;
+
+
+ParamsParameterModifier ppm  = new ParamsParameterModifier();
+ppm.paramsParameterModifier(1, 2, 3, 3, 4, 5, 6, 6, 7, 7, 8,8);
