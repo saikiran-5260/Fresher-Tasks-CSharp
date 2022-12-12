db7b473a019833bc2f4ac84aaf628c7fb036a0b3
saikiran-5260 <119442185+saikiran-5260@users.noreply.github.com>
2022-12-12 22:02:34 +0530

    Add files via upload

---
+++ Queue.cs
@@ -0,0 +1,33 @@
+﻿
+
+
+using System.Collections.Concurrent;
+
+Queue<int> q = new Queue<int>();
+
+q.Enqueue(1);
+q.Enqueue(2);
+q.Enqueue(3);
+q.Enqueue(4);
+q.Enqueue(5);
+q.Enqueue(6);
+q.Enqueue(7);
+
+foreach(int i in q)
+{
+    Console.WriteLine(i);
+}
+
+int dq = q.Dequeue();
+Console.WriteLine("dequeue: "+dq);
+
+int dq1  = q.Dequeue();
+Console.WriteLine("dequeue: "+dq1);
+
+int pk = q.Peek();
+Console.WriteLine("Peek element: "+pk);
+
+foreach(int j in q)
+{
+    Console.WriteLine(j);
+}
