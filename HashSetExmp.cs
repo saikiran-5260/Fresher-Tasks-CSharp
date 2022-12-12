62b6438144298b226eb9b9bc5c6f7640832764ca
saikiran-5260 <119442185+saikiran-5260@users.noreply.github.com>
2022-12-12 16:37:04 +0530

    Add files via upload

---
+++ HashSetExmp.cs
@@ -0,0 +1,25 @@
+﻿
+
+
+HashSet<string> hs = new HashSet<string>() { 
+    "Good Moring",
+    "Team",
+    "How did your weekend go?",
+    "Please let me know"
+};
+
+Console.WriteLine(hs.Count);
+
+hs.Add("Welcome All");
+
+foreach(string h in hs)
+{
+    Console.WriteLine(h);
+}
+
+hs.RemoveWhere(e => e.EndsWith("All"));
+Console.WriteLine("\n");
+foreach(string st in hs)
+{
+    Console.WriteLine(st);
+}
