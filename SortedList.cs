8b8a21c86abdc8d1e7d574ddb60d489912cee1db
saikiran-5260 <119442185+saikiran-5260@users.noreply.github.com>
2022-12-12 13:11:47 +0530

    Add files via upload

---
+++ SortedList.cs
@@ -0,0 +1,24 @@
+﻿
+
+
+
+SortedList<int, string> sl = new SortedList<int, string>() {
+    {48,"Telugu" },
+    {23,"Hindi" },
+    {66,"English" },
+    {99,"Maths" }
+};
+sl.Add(21, "Science");
+sl.Add(43, "Social");
+sl.Add(49, "Computers");
+sl.Remove(21);
+foreach (KeyValuePair<int,string> ele in sl)
+{
+    Console.WriteLine(ele.Key + "," + ele.Value);
+}
+
+Console.WriteLine(sl.ContainsKey(66));
+Console.WriteLine(sl.ContainsValue("Maths"));
+Console.WriteLine(sl.IndexOfKey(49));
+Console.WriteLine(sl.IndexOfValue("Hindi"));
+
