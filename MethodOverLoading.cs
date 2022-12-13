f97db42fc0f9454627e24521acc2b0cad56dca94
saikiran-5260 <119442185+saikiran-5260@users.noreply.github.com>
2022-12-13 14:58:49 +0530

    Add files via upload

---
+++ MethodOverLoading.cs
@@ -0,0 +1,41 @@
+﻿
+namespace TaskMethodOverLoading
+{
+    public class AreaExmp
+    {
+        public void Area(double l)
+        {
+            Console.WriteLine("Area of square: " + (l * l));
+        }
+      
+        public void Area(double w, int l)
+        {
+            Console.WriteLine("Area of rectangle: "+ (w*l));
+        }
+        public void Area(int h, int b)
+        {
+            Console.WriteLine("Area of triangle: " + ((b * h) / 2));
+
+        }
+
+
+    public class PerimeterExmp
+        {
+            public void Perimeter(int a)
+            {
+                Console.WriteLine("\nPerimter of square: " + (4 * a));
+            }
+            public void Perimeter(int l, int w)
+            {
+                Console.WriteLine("Perimter of rectangle: " + 2 * (l * w));
+            }
+            public void Perimeter(int a, int b, int c)
+            {
+                Console.WriteLine("Perimter of triangle : " + (a + b + c));
+
+            }
+
+        }
+       
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,16 +1,14 @@
 ﻿
 
-using ComplexTypeParameter;
+using TaskMethodOverLoading;
+using static TaskMethodOverLoading.AreaExmp;
 
-ComplexType ct = new ComplexType();
-ct.GetDetails("Shanmukha Sai Kiran Pithani", 110005260, "Hyderabad");
-ct.AddDetails(ct,"40000", "oct 17");
-ComplexType ct1= new ComplexType();
-ct1.GetDetails("Abinav Srivastava", 110005258, "Banglore");
-ct1.AddDetails(ct1, "50000","17 sep");
-ComplexType ct2 = new ComplexType();
-ct2.GetDetails("Sujitha", 110005257, "Banglore");
-ct2.AddDetails(ct2,"60000", "jun 20");
-ComplexType ct3 = new ComplexType();
-ct3.GetDetails("Praveen", 110005256, "Banglore");
-ct3.AddDetails(ct3,"70000", "jun 10");
+AreaExmp ae = new AreaExmp();
+ae.Area(6);
+ae.Area(5.6, 6);
+ae.Area(5, 6);
+
+PerimeterExmp pe = new PerimeterExmp();
+pe.Perimeter(5);
+pe.Perimeter(5, 6);
+pe.Perimeter(5, 6, 7);
