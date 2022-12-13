8f3c1945307e285afb60a4b2bb0a8c3911f2bfcd
saikiran-5260 <119442185+saikiran-5260@users.noreply.github.com>
2022-12-13 12:41:37 +0530

    Add files via upload

---
+++ ComplexType.cs
@@ -0,0 +1,33 @@
+﻿
+
+namespace ComplexTypeParameter
+{
+    public class ComplexType
+    {
+        public string employeeName="";
+        public int employeeId;
+        public string employeeLocation="";
+        public string empSalary;
+
+        public void GetDetails(string EmpName,int EmployeeId,string empLocation)
+        {
+            employeeName = EmpName;
+            employeeId = EmployeeId;
+            employeeLocation = empLocation;
+            Console.WriteLine(employeeName);
+            Console.WriteLine(employeeId);
+            Console.WriteLine(employeeLocation);
+        }
+        public void AddDetails (ComplexType complexType, params string[] p)
+        {
+            foreach(string i in p)
+            {
+                Console.WriteLine(i);
+            } 
+            Console.WriteLine("\n"+complexType.employeeName);
+            Console.WriteLine(complexType.employeeId);
+            Console.WriteLine(complexType.employeeLocation);
+            Console.WriteLine(empSalary+"\n");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿
+
+using ComplexTypeParameter;
+
+ComplexType ct = new ComplexType();
+ct.GetDetails("Shanmukha Sai Kiran Pithani", 110005260, "Hyderabad");
+ct.AddDetails(ct,"40000", "oct 17");
+ComplexType ct1= new ComplexType();
+ct1.GetDetails("Abinav Srivastava", 110005258, "Banglore");
+ct1.AddDetails(ct1, "50000","17 sep");
+ComplexType ct2 = new ComplexType();
+ct2.GetDetails("Sujitha", 110005257, "Banglore");
+ct2.AddDetails(ct2,"60000", "jun 20");
+ComplexType ct3 = new ComplexType();
+ct3.GetDetails("Praveen", 110005256, "Banglore");
+ct3.AddDetails(ct3,"70000", "jun 10");
