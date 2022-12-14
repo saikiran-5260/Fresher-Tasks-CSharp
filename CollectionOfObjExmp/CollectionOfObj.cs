01bad45954cf2facaac5da963c274f23197469fe
saikiran-5260 <119442185+saikiran-5260@users.noreply.github.com>
2022-12-14 18:45:07 +0530

    Add files via upload

---
+++ CollectionOfObjExmp/CollectionOfObj.cs
@@ -0,0 +1,14 @@
+﻿
+
+namespace CollectionOfObj
+{
+    public class Product
+    {
+        public int productID { get; set; }
+        public string productName { get; set; }
+
+        public int price { get; set; }
+
+        public DateTime dom { get; set; }
+    }
+}
---
+++ CollectionOfObjExmp/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using CollectionOfObj;
+
+
+List<Product> p = new List<Product>();
+
+string choice = "";
+do
+{
+    Product product = new Product();
+    Console.Write("Enter product id: ");
+    product.productID = int.Parse(Console.ReadLine());
+
+    Console.Write("Enter product name: ");
+    product.productName = Console.ReadLine();
+
+    Console.Write("Price of product: ");
+    product.price = int.Parse(Console.ReadLine());
+
+    Console.Write("Please enter date of manufacture in yyyy-mm-dd: ");
+    product.dom = DateTime.Parse(Console.ReadLine());
+
+    p.Add(product);
+
+    Console.Write("Please enter your choice Yes/No : ");
+    choice = Console.ReadLine();
+    Console.WriteLine("\n");
+
+} while (choice != "No" && choice != "no" && choice != "n" && choice != "NO" && choice != "nO");
+
+
+foreach (Product item in p)
+{
+    Console.WriteLine(item.productID + " " + item.productName + " " + item.price + " " + item.dom.ToShortDateString());
+}
