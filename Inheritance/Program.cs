4810d2576b61285b9010d4fc15f9fc168546803f
saikiran-5260 <119442185+saikiran-5260@users.noreply.github.com>
2022-12-14 18:44:16 +0530

    Add files via upload

---
+++ Inheritance/Program.cs
@@ -0,0 +1,48 @@
+﻿
+
+using InheritanceExmp;
+
+ShapeClass sc = new ShapeClass();
+ShapeClass.a = int.Parse(Console.ReadLine());
+ShapeClass.b = int.Parse(Console.ReadLine());
+
+Rectangle r = new Rectangle();
+r.AreaOfRectangele();
+
+Square sq = new Square();
+sq.AreaOfSquare();
+
+Circle c = new Circle();
+c.AreaOfCircle();
+
+
+//ShapeClass shapeClass= new ShapeClass();
+//shapeClass.a = int.Parse(Console.ReadLine());
+//shapeClass.b = int.Parse(Console.ReadLine());
+
+//Rectangle rectangle= new Rectangle();
+//rectangle.a = shapeClass.a;
+//rectangle.b = shapeClass.b;
+//rectangle.AreaOfRectangele();
+
+
+//Square square = new Square();
+//square.a = rectangle.a;
+//square.AreaOfSquare();
+
+//Circle circle = new Circle();   
+//circle.a = rectangle.a;
+//circle.AreaOfCircle();
+
+//Rectangle r = new Rectangle();
+//r.a= int.Parse(Console.ReadLine());
+//r.b= int.Parse(Console.ReadLine());
+//r.AreaOfRectangele();
+
+//Square sq = new Square();
+//sq.a= int.Parse(Console.ReadLine());
+//sq.AreaOfSquare();
+
+//Circle c  = new Circle();
+//c.a= int.Parse(Console.ReadLine());
+//c.AreaOfCircle();
---
+++ Inheritance/ShapeClass.cs
@@ -0,0 +1,33 @@
+﻿
+using System.Threading.Tasks;
+
+namespace InheritanceExmp
+{
+    public class ShapeClass
+    {
+        public static int a, b;
+    }
+    public class Rectangle: ShapeClass 
+    {
+        int x = 0;
+        public void AreaOfRectangele( )
+        {
+            Console.WriteLine("Area of Rectangel: {0}", a * b);
+        }
+    }
+    public class Square : ShapeClass
+    {
+        public void AreaOfSquare()
+        {
+            Console.WriteLine("Area of Square: {0}", (a * a));
+        }
+    }
+    public class Circle : ShapeClass 
+    {
+        public void AreaOfCircle()
+        {
+            Console.WriteLine("Area of circle: {0}", (3.14 * a * a));
+        }
+    }
+
+}
