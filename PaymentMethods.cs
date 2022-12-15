9b3e96a30fa0a4a5f0e4633ea1c9b2bd4e5d67bd
saikiran-5260 <119442185+saikiran-5260@users.noreply.github.com>
2022-12-15 16:59:05 +0530

    Add files via upload

---
+++ PaymentMethods.cs
@@ -0,0 +1,107 @@
+﻿
+using System;
+namespace PaymentOptionsOverLoading
+{
+    public class PaymentMethods
+    {
+        public NetBanking nb;
+        public COD c;
+        public CreditCard cc;
+        public DebitCard dc;
+        public NetBanking PaymentMethod()
+        {
+            nb = new NetBanking();
+            Console.Write("Please enter UserId: ");
+            nb.UserId = Console.ReadLine();
+            Console.Write("Please enter password: ");
+            nb.Password = Console.ReadLine();
+            Console.Write(nb.UserId + " " + nb.Password);
+            Console.WriteLine("\n");
+            
+            return nb;
+        }
+        public COD PaymentMethod(NetBanking nb)
+        {
+            c= new COD();
+            c.desc = "90% population is choosing online payment if you change your mind please folow below procedure";
+            Console.WriteLine(c.desc);
+            Console.WriteLine("\n");
+            return c;
+
+        }
+        public CreditCard PaymentMethod(NetBanking nb, COD c)
+        {
+            cc = new CreditCard();
+            Console.Write("Please enter cardNum: ");
+            cc.cardNum = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Please enter cardName: ");
+            cc.cardName = Console.ReadLine();
+
+            Console.Write("Please enter expiry Month: ");
+            cc.expiryMonth = int.Parse(Console.ReadLine());
+
+            Console.Write("Please enter expiry Year: ");
+            cc.expiryYear = int.Parse(Console.ReadLine());
+
+            Console.Write("Please enter CVV: ");
+            cc.cvv = int.Parse(Console.ReadLine());
+            Console.WriteLine(cc.cardNum + "," + cc.cardName + "," + cc.expiryMonth + "," + cc.expiryYear + "," + cc.cvv);
+            Console.WriteLine("\n");
+            return cc;
+
+        }
+        public DebitCard PaymentMethod(NetBanking nb, COD c,CreditCard cc)
+        {
+            dc = new DebitCard();
+            Console.Write("Please enter cardNum: ");
+            dc.cardNum = int.Parse(Console.ReadLine());
+
+            Console.Write("Please enter cardName: ");
+            dc.cardName = Console.ReadLine();
+
+            Console.Write("Please enter expiry Month: ");
+            dc.expiryMonth = int.Parse(Console.ReadLine());
+
+            Console.Write("Please enter expiry year: ");
+            dc.expiryYear = int.Parse(Console.ReadLine());
+
+            Console.Write("Please enter CVV: ");
+            dc.cvv = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(dc.cardNum + "," + dc.cardName+"," + dc.expiryMonth + "," + dc.expiryYear + "," + dc.cvv);
+            Console.WriteLine("\n");
+            return dc;
+
+        }
+    }
+    public class NetBanking
+    {
+        public string UserId { get; set; }
+        public string Password { get; set; }
+
+    }
+    public class COD
+    {
+        public string desc { get; set; }
+
+    }
+    public class CreditCard
+    {
+        public double cardNum { get; set; }    
+        public int expiryMonth { get; set; }
+        public int expiryYear { get; set; }
+        public string cardName { get; set; }
+        public int cvv { get; set; }
+
+    }
+    public class DebitCard
+    {
+        public int cardNum { get; set; }
+        public int expiryMonth { get; set; }
+        public int expiryYear { get; set; }
+        public string cardName { get; set; }
+        public int cvv { get; set; }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,35 +1,9 @@
-﻿using System.Collections;
-using CollectionOfObj;
+﻿
+using PaymentOptionsOverLoading;
 
+PaymentMethods pm = new PaymentMethods();
 
-List<Product> p = new List<Product>();
-
-string choice = "";
-do
-{
-    Product product = new Product();
-    Console.Write("Enter product id: ");
-    product.productID = int.Parse(Console.ReadLine());
-
-    Console.Write("Enter product name: ");
-    product.productName = Console.ReadLine();
-
-    Console.Write("Price of product: ");
-    product.price = int.Parse(Console.ReadLine());
-
-    Console.Write("Please enter date of manufacture in yyyy-mm-dd: ");
-    product.dom = DateTime.Parse(Console.ReadLine());
-
-    p.Add(product);
-
-    Console.Write("Please enter your choice Yes/No : ");
-    choice = Console.ReadLine();
-    Console.WriteLine("\n");
-
-} while (choice != "No" && choice != "no" && choice != "n" && choice != "NO" && choice != "nO");
-
-
-foreach (Product item in p)
-{
-    Console.WriteLine(item.productID + " " + item.productName + " " + item.price + " " + item.dom.ToShortDateString());
-}
+pm.PaymentMethod();
+pm.PaymentMethod(pm.nb);
+pm.PaymentMethod(pm.nb,pm.c);
+pm.PaymentMethod(pm.nb, pm.c, pm.cc);
