b29510a112a2ef07395f3d5bd8e2283f292d893f
github pushing <shanmukha.pithani@accionlabs.com>
2022-12-15 18:20:48 +0530

    Add project files.

---
+++ ComplexTypeParameter/ComplexType.cs
@@ -0,0 +1,44 @@
+﻿
+
+namespace ComplexTypeParameter
+{
+    public class ComplexType
+    {
+        public string employeeName;
+        public int employeeId;     
+        public int empSalary;
+        public Developer d;
+        
+
+        public void GetDetails(string EmpName,int EmployeeId,int salary)
+        {
+            employeeName = EmpName;
+            employeeId = EmployeeId;
+            empSalary = salary;
+            Console.WriteLine(employeeName);
+            Console.WriteLine(employeeId);
+            Console.WriteLine(empSalary);
+            
+        }
+        public Developer AddDetails(string EmpLocation, string EmpCompanyName, string EmpDesignation)
+        {
+            d= new Developer();
+            d.employeeLocation = EmpLocation;
+            d.designation = EmpDesignation;
+            d.companyName = EmpCompanyName;
+            Console.WriteLine(d.employeeLocation);
+            Console.WriteLine(d.companyName);
+            Console.WriteLine(d.designation);
+            Console.WriteLine("\n");
+            return d;
+            
+        }
+    }
+    public class Developer
+    {
+        public string designation;
+        public string companyName;
+        public string employeeLocation;
+
+    }
+}
---
+++ ComplexTypeParameter/Program.cs
@@ -0,0 +1,20 @@
+﻿
+
+using ComplexTypeParameter;
+
+ComplexType ct = new ComplexType();
+ct.GetDetails("Shanmukha Sai Kiran Pithani", 110005260,40000);
+ct.AddDetails(EmpLocation:"Hyderabad", EmpCompanyName: "AccionLabs" , EmpDesignation: "Developer");
+ComplexType ct1= new ComplexType();
+ct1.GetDetails("Abinav Srivastava", 110005258, 50000);
+ct1.AddDetails(EmpLocation: "Banglore", EmpCompanyName: "AccionLabs", EmpDesignation: "Developer");
+ComplexType ct2 = new ComplexType();
+ct2.GetDetails("Sujitha", 110005257, 60000);
+ct2.AddDetails(EmpLocation: "Banglore", EmpCompanyName: "AccionLabs", EmpDesignation: "Developer");
+ComplexType ct3 = new ComplexType();
+ct3.GetDetails("Praveen", 110005256, 70000);
+ct3.AddDetails(EmpLocation: "Banglore", EmpCompanyName: "AccionLabs", EmpDesignation: "Developer");
+
+
+//Student student = new Student();
+//student.GetDetails(10, 20);
