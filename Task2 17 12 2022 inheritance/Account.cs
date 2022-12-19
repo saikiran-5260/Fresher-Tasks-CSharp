ba7c2dab4657d9a4874927145e1b4233c5b2f548
saikiran-5260 <119442185+saikiran-5260@users.noreply.github.com>
2022-12-19 11:37:52 +0530

    Add files via upload

---
+++ Task2 17 12 2022 inheritance/Account.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task217122022
+{
+    public class Account
+    {
+        protected int _accId, _accBalance;
+        protected string _name,_emailId;
+
+        public int Id
+        {
+            get
+            {
+                return _accId;
+            }
+        }
+        public string Name
+        {
+            get
+            {
+                return _name;
+
+            }
+            set { _name = value; }
+
+
+        }
+        public string EmailId
+        {
+            get
+            {
+                return _emailId;
+            }
+            set { _emailId = value; }
+        }
+        public void Deposite(int amount)
+        {
+            Console.WriteLine("Deposite method in Account class");
+            _accBalance = _accBalance + amount;
+            Console.WriteLine("Account balance after deposite is: {0}",_accBalance);
+        }
+        public void DisplayDetails()
+        {
+            Console.WriteLine("Account Id: {0} \nAccount Name: {1} ", _accId, Name);
+            Console.WriteLine("Account Balance: {0}",_accBalance);
+        }
+    }
+}
---
+++ Task2 17 12 2022 inheritance/Program.cs
@@ -0,0 +1,8 @@
+﻿
+using Task217122022;
+
+SalaryAccount s1= new SalaryAccount(10000, "sai kiran", "kiran@gmail.com");
+s1.Deposite(500);
+s1.DisplayDetails();
+s1.Withdraw(10000);
+s1.Transfer();
