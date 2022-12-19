12f0e6b85b5b7dcdddd23a13ff945ab31fe384bf
saikiran-5260 <119442185+saikiran-5260@users.noreply.github.com>
2022-12-19 11:36:44 +0530

    Add files via upload

---
+++ Task1 17 12 2022 properties/Program.cs
@@ -0,0 +1,16 @@
+﻿
+using Task1;
+
+Properties p = new Properties(12345,100000,"sai kiran", "kiran@gmail.com");
+p.DisplayDetails();
+int balance = p.accBalance;
+
+if(balance == -100)
+{
+    Console.WriteLine("Entered password is invalid");
+}
+else
+{
+    Console.WriteLine(balance);
+}
+Console.WriteLine(p.accId);
---
+++ Task1 17 12 2022 properties/Properties.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    public class Properties
+    {
+        private int AccId,AccBalance;
+        private string Name, EmailId;
+        
+
+        public int accId
+        {
+            get
+            {
+                Console.WriteLine("Get block is to return the value");
+                int id = AccId;
+                return id;
+                //return AccId;
+            }
+        }
+        public Properties(int accId, int accBalance, string name, string emailId)
+        {
+            AccId = accId;
+            AccBalance = accBalance;
+            Name = name;
+            EmailId = emailId;
+        }
+        public int accBalance
+        {
+            get
+            {
+                Console.WriteLine("Tyring to view my account balance");
+                Console.WriteLine("Please enter the password");
+                string pwd = Console.ReadLine();
+                if(pwd == "12345")
+                {
+                    int _accBalance = AccBalance;
+                    return _accBalance;
+                    //return AccBalance;
+                }
+                else
+                {
+                    return -100;
+                }
+            }
+        }
+        public void DisplayDetails()
+        {
+            Console.WriteLine("Account details are: ");
+            Console.WriteLine("Account number: " + AccId);
+            Console.WriteLine("Account name: " + Name);
+            Console.WriteLine("Account balance is: " + AccBalance);
+            Console.WriteLine("Account holder emailid : " + EmailId);
+        }
+    }
+}
