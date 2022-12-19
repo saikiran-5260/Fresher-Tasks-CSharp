eff6c211bcfead165ad0a422db1c0811f99a1ae0
saikiran-5260 <119442185+saikiran-5260@users.noreply.github.com>
2022-12-19 11:35:53 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,7 +1,8 @@
 ﻿
+using Task217122022;
 
-
-using SingleTonClass;
-
-SingleTon s = SingleTon.Instance;
-s.DisplayDetails();
+SalaryAccount s1= new SalaryAccount(10000, "sai kiran", "kiran@gmail.com");
+s1.Deposite(500);
+s1.DisplayDetails();
+s1.Withdraw(10000);
+s1.Transfer();
---
+++ SalaryAccount.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task217122022
+{
+    public class SalaryAccount:Account
+    {
+        public SalaryAccount(int intialBalcance, string name,string emailId) 
+        {
+            _accId = 99898989;
+            _accBalance= intialBalcance;
+            _name = name;
+            _emailId = emailId;
+
+        }
+        public void Withdraw(int amount)
+        {
+            Console.WriteLine("\nWithdraw method in salary account");
+            
+            if(amount>=_accBalance)
+            {
+                Console.WriteLine("Insufficient funds in account: {0}", _accBalance);
+                Console.WriteLine("Please try amount less than account balance");
+            }
+            else
+            {
+                Console.Write("Please enter your 5 digit pin: ");
+                string Pin = Console.ReadLine();
+                if (Pin == "12345")
+                {
+                    _accBalance = _accBalance - amount;
+                    Console.WriteLine("Withdraw successful");
+                    Console.WriteLine("Blance amount after withdraw is: {0}",_accBalance);
+
+                }
+                else
+                {
+                    Console.WriteLine("Pin incorrect please try again");
+                }
+            }
+        }
+        public void Transfer()
+        {
+            Console.WriteLine("\nTransfer method in salary account");
+            Console.Write("Enter amount to be transfer: ");
+            int transferAmt = int.Parse(Console.ReadLine());
+            if (transferAmt>=_accBalance)
+            {
+                Console.WriteLine("Transfer Amount is greater than account balance\nInsufficient funds" +
+                    "\nYour available balance is: {0}", _accBalance);
+            }
+            else
+            {
+                Console.Write("Please enter your 5 digit pin: ");
+                string Pin = Console.ReadLine();
+                if (Pin == "54321") 
+                { 
+                    _accBalance = _accBalance - transferAmt;
+                    Console.WriteLine("Transfer successful");
+                    Console.WriteLine("Your account balance after transfer is: {0}", _accBalance);
+                }
+                else
+                {
+                    Console.WriteLine("Pin incorrect");
+                }
+            }
+
+        }
+    }
+}
