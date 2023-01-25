481cb571f88a4c598ec1b6904ed98a076f3ca6e6
saikiran-5260 <119442185+saikiran-5260@users.noreply.github.com>
2023-01-25 13:00:58 +0530

    Add files via upload

---
+++ ICloneableInterfaceImplementation/ICloneableInterfaceImplementation/Program.cs
@@ -0,0 +1,12 @@
+﻿
+
+
+//string Myname = "Sai";
+//string clone = (string)Myname.Clone();
+
+using ICloneableInterfaceImplementation;
+
+sample s = new sample();
+s.Name = "Sai";
+sample clone = (sample)s.Clone();
+Console.WriteLine(clone.Name);
---
+++ ICloneableInterfaceImplementation/ICloneableInterfaceImplementation/sample.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ICloneableInterfaceImplementation
+{
+    public class sample:ICloneable
+    {
+        internal protected string Name { get; set; }
+
+        public object Clone()
+        {
+            return this;
+        }
+    }
+}
