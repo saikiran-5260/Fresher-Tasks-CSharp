c2b68ac2d8f795e0b3c56d7ddf203cc27e6c293b
saikiran-5260 <119442185+saikiran-5260@users.noreply.github.com>
2023-01-30 11:13:34 +0530

    Add files via upload

---
+++ Delegates/Delegate.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateExmp
+{
+    delegate void del(int x, int y);
+    public class Delegate
+    {
+        public static void Add(int x,int y)
+        {
+            Console.WriteLine(x+y);
+
+        }
+        public static void Mul(int x,int y)
+        {
+            Console.WriteLine(x * y);
+        }
+    }
+}
---
+++ Delegates/Program.cs
@@ -0,0 +1,13 @@
+﻿
+
+
+using DelegateExmp;
+
+DelegateExmp.Delegate dl = new();
+del d = new(DelegateExmp.Delegate.Add);
+d += DelegateExmp.Delegate.Mul;
+d(10, 20);
+
+
+
+
