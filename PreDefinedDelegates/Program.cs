1ef24524cf03a160d0690b218d73e61db1411f4e
saikiran-5260 <119442185+saikiran-5260@users.noreply.github.com>
2023-01-30 11:14:58 +0530

    Add files via upload

---
+++ PreDefinedDelegates/Program.cs
@@ -0,0 +1,27 @@
+﻿
+
+using PreDefinedDelegates;
+
+//Sample s = new Sample();
+//AddNumbers add = Sample.AddNumbers;
+//double result = add.Invoke(10, 20.6f, 65.76);
+//Console.WriteLine(result);
+
+//AddNumbers1 add1 = Sample.AddNumbers1;
+//add1.Invoke(10, 20.6f, 65.76);
+
+//Status s1 = Sample.Status;
+//bool status = s1.Invoke("Hello");
+//Console.WriteLine(status);
+
+Func<int, float, double, double> func = Sample.AddNumbers;
+double result = func.Invoke(10, 20.6f, 65.76);
+Console.WriteLine(result);
+
+Action<int, float, double> ac = Sample.AddNumbers1;
+ac.Invoke(10, 20.6f, 65.76);
+
+Predicate<string> p = Sample.Status;
+bool result2 = p.Invoke("Sai");
+Console.WriteLine(result2);
+
---
+++ PreDefinedDelegates/Sample.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PreDefinedDelegates
+{
+    //public delegate double AddNumbers(int x, float y,double z);
+    //public delegate void AddNumbers1(int x, float y, double z);
+    //public delegate bool Status(string str);
+    public class Sample
+    {
+        public static double AddNumbers(int x, float y,double z)
+        {
+            return x + y + z;
+        }
+        public static void AddNumbers1(int x, float y,double z) 
+        {
+            Console.WriteLine("Addition of three numbers: "+(x+y+z));
+        }
+        public static bool Status(string str)
+        {
+            if(str.Length>5)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+    }
+}
