0e8864fe2b1ca14fecf55b9f4a73c07cb5a39491
saikiran-5260 <119442185+saikiran-5260@users.noreply.github.com>
2023-01-30 11:11:46 +0530

    Add files via upload

---
+++ AnonymousMethod/Program.cs
@@ -0,0 +1,12 @@
+﻿
+
+using AnonymousMethod;
+//Anonymous Method
+myDel del = delegate(int x, int y)
+{
+    return x + y; 
+};
+
+
+int result = del.Invoke(5, 6);
+Console.WriteLine(result);
---
+++ AnonymousMethod/sample.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnonymousMethod
+{
+    public delegate int myDel(int x,int y);
+    public class sample
+    {
+       
+
+    }
+}
