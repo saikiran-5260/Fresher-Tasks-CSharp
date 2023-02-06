99c6aa592a3d36656181cb44c4bf82c50bac3322
saikiran-5260 <119442185+saikiran-5260@users.noreply.github.com>
2023-02-06 22:42:31 +0530

    Add files via upload

---
+++ MultiThreading/MultiThreading/Program.cs
@@ -0,0 +1,18 @@
+﻿
+
+
+using MultiThreading;
+using System.Threading;
+
+ThreadingMethods tm = new();
+Thread T1 = new Thread(tm.Thread1);
+Thread T2 = new Thread(tm.Thread2);
+Thread T3 = new Thread(tm.Thread3);
+
+T1.Start();
+T2.Start();
+T3.Start();
+
+T1.Join();
+T2.Join();
+T3.Join();
---
+++ MultiThreading/MultiThreading/ThreadingMethods.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiThreading
+{
+    public class ThreadingMethods
+    {
+        public void Thread1() 
+        {
+            for(int i =1;i<=10;i++)
+            {
+                Console.WriteLine("Thread1:"+i);
+            }
+        }
+        public void Thread2()
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine("Thread2:" + i);
+                if(i==5)
+                {
+                    Console.WriteLine("Thread is sleeping");
+                    Thread.Sleep(5000);
+                    Console.WriteLine("Thread woke up");
+                }   
+            }
+        }
+        public void Thread3()
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine("Thread3:" + i);
+            }
+        }
+    }
+}
