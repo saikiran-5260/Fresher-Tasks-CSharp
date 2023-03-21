bc156a0453a4548c694affb7e334842598682c81
saikiran-5260 <119442185+saikiran-5260@users.noreply.github.com>
2023-03-21 13:11:58 +0530

    Add files via upload

---
+++ Practical 2/Practical 2/ClassB.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practical_2
+{
+    public class ClassB
+    {
+        //public void AdamNum()
+        //{
+        //    int num = int.Parse(Console.ReadLine());
+        //    int revNum = Reverse(num);
+        //    bool IsAdam = Math.Pow(num, 2) == Reverse((int)Math.Pow(revNum, 2));
+        //    if (IsAdam == true)
+        //    {
+        //        Console.WriteLine("The given number is adam");
+        //    }
+        //    else
+        //    {
+        //        Console.WriteLine("Not an adam number");
+        //    }
+        //}
+        
+        //private int Reverse(int num)
+        //{
+        //    int result = 0;
+        //    while (num > 0)
+        //    {
+        //        result = result * 10 + num % 10;
+        //        num /= 10;
+        //    }
+        //    return result;
+        //}
+
+        //public void Factorial()
+        //{
+        //    int fact = 1;
+        //    int number = int.Parse(Console.ReadLine());
+        //    for(int i = 1; i <= number; i++)
+        //    {
+        //        fact *= i;
+        //    }
+        //    Console.WriteLine("The factorial of given number{0} is {1}",number,fact);
+        //}
+        //public void Fibonacci()
+        //{
+        //    int number = int.Parse(Console.ReadLine());
+        //    int n1 = 0, n2 = 1, n3;
+        //    Console.Write(n1+","+n2+",");
+        //    for(int i = 2; i <= number; i++)
+        //    {
+        //        n3 = n1 + n2;
+        //        Console.Write(n3+",");
+        //        n1 = n2;
+        //        n2 = n3;
+        //    }
+        //}
+        public void StringReverse()
+        {
+            string str = Console.ReadLine();
+            char[] chars = str.ToCharArray();
+            Array.Reverse(chars);
+            Console.WriteLine("Reverse of string: "+chars);
+            string s = new string(chars);
+            Console.WriteLine(s);
+
+
+        }
+    }
+}
---
+++ Practical 2/Practical 2/Program.cs
@@ -0,0 +1,74 @@
+﻿
+
+////Prime Number
+//int count = 0;
+//int num =int.Parse(Console.ReadLine());
+//for(int i = 1; i <= num; i++)
+//{
+//    if (num % i == 0)
+//    {
+//        count++;
+//    }
+//}
+//if (count == 2)
+//{
+//    Console.WriteLine("The give number is prime");
+//}
+//else
+//{
+//    Console.WriteLine("Not prime");
+//}
+
+//Amstrong
+//int num1 = int.Parse(Console.ReadLine());
+//int n;
+//double c = 0, sum = 0;
+//n = num1; int temp = num1;
+//while (n != 0)
+//{
+//    c = c + 1;
+//    n /= 10;
+//}
+//while (num1 > 0)
+//{
+//    int r = num1 % 10;
+//    sum = sum + Math.Pow(r, c);
+//    num1 /= 10;
+//}
+//if (temp == sum)
+//{
+//    Console.WriteLine("The given number is amstrong");
+//}
+//else
+//{
+//    Console.WriteLine("Not Amstrong");
+//}
+
+//Palindrome
+//int num = int.Parse(Console.ReadLine());
+//int temp = num,sum=0;
+//while(num> 0)
+//{
+//    sum = sum * 10 + num % 10;
+//    num/= 10;
+//}
+//if (temp == sum)
+//{
+//    Console.WriteLine("The given number is palindrome");
+//}
+//else
+//{
+//    Console.WriteLine("Not a plindrome");
+//}
+
+//Adam Number
+
+using Practical_2;
+
+ClassB b = new ClassB();
+//b.AdamNum();
+//b.Factorial();
+//b.Fibonacci();
+b.StringReverse();
+
+
